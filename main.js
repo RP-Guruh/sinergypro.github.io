8bd1e2e7ea8a633d9c607d2c5347cd9f9480b29f
Guruh Rachmat <46945652+RP-Guruh@users.noreply.github.com>
2019-10-15 16:08:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
+    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
